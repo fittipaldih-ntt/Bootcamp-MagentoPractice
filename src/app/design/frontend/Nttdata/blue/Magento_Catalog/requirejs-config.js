076a65961fb10014c6b75199c6603eb7e7da76f0
fittipaldih-ntt <hfittipa@emeal.nttdata.com>
2023-09-19 14:05:23 -0300

    Ejercicio 7.1 - RefactorYBonusCarousel

---
+++ src/app/design/frontend/Nttdata/blue/Magento_Catalog/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        'jquery.ui.widget': 'Nttdata/js/jquery.ui.widget'
+    },
+    shim: {
+        'jquery.ui.widget': {
+            'deps': ['jquery']
+        }
+    }
+};

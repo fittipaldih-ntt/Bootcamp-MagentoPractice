4c39765e0663e42116784709914e032f63c2d15d
fittipaldih-ntt <hfittipa@emeal.nttdata.com>
2023-09-21 12:34:57 -0300

    Ejercicio 7.3 - Ui Components(A Revisar)

--- src/app/design/frontend/Nttdata/blue/Magento_Catalog/requirejs-config.js
+++ src/app/design/frontend/Nttdata/blue/Magento_Catalog/requirejs-config.js
@@ -11,7 +11,7 @@ var config = {
 
     map: {
         "*": {
-            "accordion": "Magento_Catalog/js/accordion"
+            "accordionJS": "Magento_Catalog/js/accordion",
         }
     }
 
